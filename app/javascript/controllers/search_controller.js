c2de41445941bf78ae532ece1a11820dd0d4e24a
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-07-06 18:36:36 +0200

    autocomplete ok back

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,14 +1,46 @@
 import { Controller } from "@hotwired/stimulus";
 
+const professions = [
+  "Médecin",
+  "Pharmacie",
+  "Dentiste",
+  "Urgences"
+];
+
+const removeAccents = (str) => {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+};
+
 export default class extends Controller {
-  static targets = ["input"];
+  static targets = ["input", "results"];
+
+  connect() {
+    this.inputTarget.addEventListener("click", this.handleClick.bind(this));
+    this.inputTarget.addEventListener("input", this.handleInput.bind(this));
+  }
+
+  handleClick(event) {
+    this.displaySuggestions(professions);
+  }
+
+  handleInput(event) {
+    const query = removeAccents(event.target.value);
+    const filteredProfessions = professions.filter(profession => removeAccents(profession).startsWith(query));
+
+    this.displaySuggestions(filteredProfessions);
+  }
+
+  displaySuggestions(professions) {
+    const resultsHTML = professions.map(profession => `<li class="list-group-item" role="option">${profession}</li>`).join("");
+    this.resultsTarget.innerHTML = resultsHTML;
+  }
 
   clear() {
-    this.inputTarget.value = ""; // Efface le champ de recherche
-    this.submitForm(); // Déclenche une nouvelle recherche
+    this.inputTarget.value = "";
+    this.resultsTarget.innerHTML = "";
   }
 
   submitForm() {
-    this.element.submit(); // Soumet le formulaire
+    this.element.submit();
   }
 }
