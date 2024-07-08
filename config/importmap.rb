b3833680febf3705b20ca0bb8da244952b76b38a
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-07-08 17:47:40 +0200

    config geocoder mapbox autocomplete ok, manque input searchbar

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
+pin "@mapbox/mapbox-gl-geocoder", to: "https://ga.jspm.io/npm:@mapbox/mapbox-gl-geocoder@5.0.2/dist/mapbox-gl-geocoder.min.js"
+
