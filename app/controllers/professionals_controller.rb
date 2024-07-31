a23e6dc782b1607f7d998921dfb19c8683f614c9
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-07-31 18:42:18 +0200

    open now partial + open now logique annuaire.html ok

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -2,9 +2,16 @@ class ProfessionalsController < ApplicationController
   def index
     @professionals = Professional.all
     @query_city = params[:query_city]
+    @open_now = params[:open_now] == 'true'
+
     if @query_city.present?
       @professionals = @professionals.search_by_city(@query_city)
     end
+
+    if @open_now
+      @professionals = @professionals.to_a.select(&:open_now?)
+    end
+
     render :annuaire
   end
 
