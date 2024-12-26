4db7d9fcbc0283cfd558ab45ad35a184eef7efb1
Amin Sanouj <aminsanouj.dev@gmail.com>
2024-12-26 01:14:03 +0100

    Front Update

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -9,6 +9,7 @@ class ContactController < ApplicationController
     # Vérification des champs requis
     if name.blank? || email.blank? || subject.blank? || message.blank?
       flash[:contact_notice] = "Tous les champs doivent être remplis."
+      flash[:contact_notice_type] = "error"
       flash[:contact_form] = { name: name, email: email, subject: subject, message: message }
       redirect_to contact_path and return
     end
@@ -16,6 +17,7 @@ class ContactController < ApplicationController
     # Envoi de l'e-mail via le mailer
     ContactMailer.contact_email(name, email, subject, message).deliver_now
     flash[:contact_notice] = "Votre message a bien été envoyé."
+    flash[:contact_notice_type] = "success"
     redirect_to contact_path
   end
 end
